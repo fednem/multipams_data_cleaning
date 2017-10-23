0256fba9b29699e4c8cb5c3d32bbcfaab2bf8180
fednem <federico.nemmi@gmail.com>
2017-10-23 15:12:24 +0200

    fixed data cleaning and advanced analysis

--- data_cleaning_script.s
+++ data_cleaning_script.s
@@ -13,13 +13,14 @@ park_demographic_info <- park_demographic_info %>%
   select(code = N..inclusion, sex = sexe, hand = Droitier.Gaucher, date_of_birth = ddn, 
          stad_park = Stade.parkinson.en.ON, desease_start = date.début.maladie,
          worse_side = côté.le.plus.atteint.de.la.maladie) %>%
-  slice(-1)
+  slice(-1) %>%
+  mutate(code = gsub("-","_",code))
 
 park_first_visit <- park_first_visit %>%
   mutate(code = park_demographic_info$code) %>%
   select(code, date_mri = Date.de.l.IRM, age = Age.au.moment.de.l.IRM, mmse = MMSE...30.,
          updrs = Score.UPDRS, beck = Score.Beck...63., moca = Score.Moca...30., scopa = Score.scopa...69.)  %>%
-  mutate(moca = as.numeric(moca))
+mutate(moca = as.numeric(moca), code = gsub("-","_",code))
 
 
 park_second_visit <- park_second_visit %>%
@@ -39,7 +40,9 @@ pams_second_visit[30,] <- NA
 pams_demographic_info <- pams_demographic_info %>%
   select(code = N..inclusion, ams = Type.d.AMS, sex = sexe, hand = Droitier.Gaucher, date_of_birth = ddn,
          msa_diagnostic_level = diagnostic.MSA, deseaese_start = date.début.maladie, worse_side = côté.le.plus.atteint) %>%
-  slice(-1)
+  slice(-1)%>%
+  mutate(code = gsub("-","_",code))
+
 
 pams_first_visit <- pams_first_visit %>%
   mutate(code = pams_demographic_info$code) %>%
@@ -74,11 +77,11 @@ hc_final <- bind_cols(hc_code, hc_sex_dob) %>%
   mutate(visit = as.character(1)) %>%
   select(visit, code = N..inclusion, sex = sexe, date_of_birth = date.de..naissance, date_mri = Date.de.l.IRM,
          age = Age.au.moment.de.l.IRM, mmse = MMSE...30., beck = Dépression.de..Beck...63., scopa = SCOPA) %>%
-  mutate(beck = as.numeric())
+  mutate(beck = as.numeric(beck))
 
-shared_variables <- intersect(colnames(park_final), colnames(pams_final)) %>%
-  intersect(colnames(hc_final)) 
-  
+shared_variables <- intersect(colnames(park_pams_final), colnames(hc_final))
+shared_variables <- shared_variables[which(shared_variables != "group")]
+
 
 park_pams_hc_final <- bind_rows(select(park_final, shared_variables), 
                                 select(pams_final, shared_variables), 
