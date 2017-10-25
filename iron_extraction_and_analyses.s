a2ddbbc9c48c32510db2a2d7c9d14c5fe0fd1056
fednem <federico.nemmi@gmail.com>
2017-10-25 10:48:46 +0200

    checked results using different ROIs (withiut success). Added extraction method for all the roi as well as analysis to compare values from different rois

--- iron_extraction_and_analyses.s
+++ iron_extraction_and_analyses.s
@@ -64,6 +64,7 @@ print(bar_plot_iron_gaetano_by_group)
 #correlate iron from your method to Germaine's
 
 iron_germaine <- read_delim("germaine_value.txt", delim = "\t")
+subjects_germaine <- scan("ger_pat.txt", "%s%")
 
 correlation_iron_mine_iron_germaine <- right_join(mat, iron_germaine, by = "code") %>%
     with(., cor.test(iron, value))
@@ -77,6 +78,7 @@ right_join(mat, iron_germaine, by = "code") %>%
   stat_smooth(method = "lm")
 
 
+#correlate gaetano's and germaine's values
 correlation_iron_gaetano_iron_germaine <- inner_join(filter(park_pams_final_plus_gateano_dataset, 
                                                         visit == 1), iron_germaine, by = "code") %>%
   with(., cor.test(value, SN_r2s))
@@ -84,6 +86,157 @@ correlation_iron_gaetano_iron_germaine <- inner_join(filter(park_pams_final_plus
 inner_join(filter(park_pams_final_plus_gateano_dataset, 
                   visit == 1), iron_germaine, by = "code") %>%
   with(., plot(value, SN_r2s))
+
+#check values from sn_bilateral_Pat_eroded
+
+load("iron_data_eroded_roi.RData")
+
+iron_data_eroded_roi <- iron_data_eroded_roi[-24]
+
+mat$iron_eroded <- as.numeric(iron_data_eroded_roi)
+
+correlation_non_eroded_eroded_value <- cor.test(mat$iron, mat$iron_eroded)
+
+plot(mat$iron, mat$iron_eroded)
+
+correlation_iron_gaetano_iron_eroded <- left_join(mat, park_pams_final_plus_gateano_dataset, by = "code") %>%
+  filter(visit == 1) %>%
+  with(., cor.test(iron_eroded, SN_r2s))
+
+left_join(mat, park_pams_final_plus_gateano_dataset, by = "code") %>%
+  filter(visit == 1) %>%
+  with(., plot(iron_eroded, SN_r2s))
+
+correlation_iron_eroded_iron_germaine <- right_join(mat, iron_germaine, by = "code") %>%
+  with(., cor.test(iron_eroded, value))
+
+right_join(mat, iron_germaine, by = "code") %>%
+  with(., plot(iron_eroded, value))
+
+# check group difference with eroded values
+
+group_effect_on_iron_eroded <- oneway.test(iron_eroded ~ group, data = mat)
+
+park_vs_hc_iron_eroded <- t.test(mat$iron_eroded[group == "group_1"],
+                                 mat$iron_eroded[group == "group_3"])
+
+park_vs_hc_iron_eroded_germaine_subsample <- filter(mat, code %in% subjects_germaine | group == "group_3") %>%
+  t.test(iron_eroded ~ group, data = .)
+
+
+#check values from the avg of left and right sn_*_pat
+
+load("iron_data_avg_left_right_roi.RData")
+
+iron_data_avg_left_right_roi <- iron_data_avg_left_right_roi[-24]
+
+mat$iron_avg_left_right_sn <- as.numeric(iron_data_avg_left_right_roi)
+
+correlation_iron_avg_left_and_right_value <- cor.test(mat$iron, mat$iron_avg_left_right_sn)
+
+plot(mat$iron, mat$iron_avg_left_right_sn)
+
+correlation_iron_gaetano_iron_avg_left_right_sn <- left_join(mat, park_pams_final_plus_gateano_dataset, by = "code") %>%
+  filter(visit == 1) %>%
+  with(., cor.test(iron_avg_left_right_sn, SN_r2s))
+
+left_join(mat, park_pams_final_plus_gateano_dataset, by = "code") %>%
+  filter(visit == 1) %>%
+  with(., plot(iron_avg_left_right_sn, SN_r2s))
+
+correlation_iron_avg_left_right_sn_iron_germaine <- right_join(mat, iron_germaine, by = "code") %>%
+  with(., cor.test(iron_avg_left_right_sn, value))
+
+right_join(mat, iron_germaine, by = "code") %>%
+  with(., plot(iron_avg_left_right_sn, value))
+
+# check group difference with eroded values
+
+group_effect_on_iron_avg_left_right_sn <- oneway.test(iron_avg_left_right_sn ~ group, data = mat)
+
+park_vs_hc_iron_avg_left_right_sn <- t.test(mat$iron_avg_left_right_sn[group == "group_1"],
+                                 mat$iron_avg_left_right_sn[group == "group_3"])
+
+park_vs_hc_iron_avg_left_right_sn_germaine_subsample <- filter(mat, code %in% subjects_germaine | group == "group_3") %>%
+  t.test(iron_avg_left_right_sn ~ group, data = .)
+
+
+#check values from MNI 2mm roi
+
+load("iron_data_mni_2mm_roi.RData")
+
+iron_data_mni_2mm_roi <- iron_data_mni_2mm_roi[-24]
+
+mat$iron_mni_2mm <- as.numeric(iron_data_mni_2mm_roi)
+
+correlation_iron_mni_2mm <- cor.test(mat$iron, mat$iron_mni_2mm)
+
+plot(mat$iron, mat$iron_mni_2mm)
+
+correlation_iron_gaetano_iron_mni_2mm <- left_join(mat, park_pams_final_plus_gateano_dataset, by = "code") %>%
+  filter(visit == 1) %>%
+  with(., cor.test(iron_mni_2mm, SN_r2s))
+
+left_join(mat, park_pams_final_plus_gateano_dataset, by = "code") %>%
+  filter(visit == 1) %>%
+  with(., plot(iron_mni_2mm, SN_r2s))
+
+correlation_iron_mni_2mm_iron_germaine <- right_join(mat, iron_germaine, by = "code") %>%
+  with(., cor.test(iron_mni_2mm, value))
+
+right_join(mat, iron_germaine, by = "code") %>%
+  with(., plot(iron_mni_2mm, value))
+
+# check group difference with eroded values
+
+group_effect_on_iron_mni_2mm <- oneway.test(iron_mni_2mm ~ group, data = mat)
+
+park_vs_hc_iron_mni_2mm <- t.test(mat$iron_mni_2mm[group == "group_1"],
+                                            mat$iron_mni_2mm[group == "group_3"])
+
+park_vs_hc_iron_mni_2mm_germaine_subsample <- filter(mat, code %in% subjects_germaine | group == "group_3") %>%
+  t.test(iron_mni_2mm ~ group, data = .)
+
+
+
+#check values from Wu 2012
+
+load("iron_data_Wu_roi.RData")
+
+iron_data_Wu_roi <- iron_data_Wu_roi[-24]
+
+mat$Wu_roi <- as.numeric(iron_data_Wu_roi)
+
+correlation_Wu_roi <- cor.test(mat$iron, mat$Wu_roi)
+
+plot(mat$iron, mat$Wu_roi)
+
+correlation_iron_gaetano_Wu_roi <- left_join(mat, park_pams_final_plus_gateano_dataset, by = "code") %>%
+  filter(visit == 1) %>%
+  with(., cor.test(Wu_roi, SN_r2s))
+
+left_join(mat, park_pams_final_plus_gateano_dataset, by = "code") %>%
+  filter(visit == 1) %>%
+  with(., plot(Wu_roi, SN_r2s))
+
+correlation_Wu_roi_iron_germaine <- right_join(mat, iron_germaine, by = "code") %>%
+  with(., cor.test(Wu_roi, value))
+
+right_join(mat, iron_germaine, by = "code") %>%
+  with(., plot(Wu_roi, value))
+
+# check group difference with eroded values
+
+group_effect_on_Wu_roi <- oneway.test(Wu_roi ~ group, data = mat)
+
+park_vs_hc_Wu_roi <- t.test(mat$Wu_roi[group == "group_1"],
+                                  mat$Wu_roi[group == "group_3"])
+
+park_vs_hc_Wu_roi_germaine_subsample <- filter(mat, code %in% subjects_germaine | group == "group_3") %>%
+  t.test(Wu_roi ~ group, data = .)
+
+
+
 
 
 value_iron_nuisance_by_rois <- mat %>%
--- iron_extraction_from_MNI_space.s
+++ iron_extraction_from_MNI_space.s
@@ -1,5 +1,5 @@
 library(fslr)
-#note that the first part of this script must be run in ubuntu
+#note that this script must be run in ubuntu
 #create 3d images of r2star and extract the value
 setwd("/mnt/d/MultiPAMS/iron/Image/")
 system('fslmerge -t 4d_normalized_r2s w*r2*')
@@ -7,3 +7,26 @@ iron_data <- system('fslstats -t 4d_normalized_r2s.nii.gz -k /mnt/d/MultiPAMS/rs
 save(file = "iron_data.RData", iron_data)
 #from here the script can be run in windows
 system("cp iron_data.RData /mnt/d/MultiPAMS/clinical_data/script/data_cleaning_github")
+
+#extract from eroded version of the roi
+iron_data_eroded_roi <- system('fslstats -t 4d_normalized_r2s.nii.gz -k /mnt/d/MultiPAMS/rs-fmri/ROIs/sn_bilateral_Pat_eroded.nii -M', intern = TRUE)
+save(file = "iron_data_eroded_roi.RData", iron_data_eroded_roi)
+
+system("cp iron_data_eroded_roi.RData /mnt/d/MultiPAMS/clinical_data/script/data_cleaning_github")
+
+#extract from left and right separately and avg, as germaine did
+iron_data_left_roi <- system('fslstats -t 4d_normalized_r2s.nii.gz -k /mnt/d/MultiPAMS/rs-fmri/ROIs/sn_l_mask_Pat.nii -M', intern = TRUE)
+iron_data_right_roi <- system('fslstats -t 4d_normalized_r2s.nii.gz -k /mnt/d/MultiPAMS/rs-fmri/ROIs/sn_r_mask_Pat.nii -M', intern = TRUE)
+iron_data_avg_left_right_roi <- (as.numeric(iron_data_right_roi) + as.numeric(iron_data_left_roi))/2
+save(file = "iron_data_avg_left_right_roi.RData", iron_data_avg_left_right_roi)
+system("cp iron_data_avg_left_right_roi.RData /mnt/d/MultiPAMS/clinical_data/script/data_cleaning_github")
+
+#extract from mni sn roi and from Wu et al roi
+iron_data_mni_2mm_roi <- system('fslstats -t 4d_normalized_r2s.nii.gz -k /mnt/d/MultiPAMS/rs-fmri/ROIs/substantia_nigra_bilateral_2mm.nii -M', intern = TRUE)
+save(file = "iron_data_mni_2mm_roi.RData", iron_data_mni_2mm_roi)
+system("cp iron_data_mni_2mm_roi.RData /mnt/d/MultiPAMS/clinical_data/script/data_cleaning_github")
+
+
+iron_data_Wu_roi <- system('fslstats -t 4d_normalized_r2s.nii.gz -k /mnt/d/MultiPAMS/rs-fmri/ROIs/bilateral_SN_Wu_2012.nii -M', intern = TRUE)
+save(file = "iron_data_Wu_roi.RData", iron_data_Wu_roi)
+system("cp iron_data_Wu_roi.RData /mnt/d/MultiPAMS/clinical_data/script/data_cleaning_github")
